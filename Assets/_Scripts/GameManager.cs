942b62b7603b17c6f6f2e55a249e947fb4e37faa
uman445 <144501257+uman445@users.noreply.github.com>
2026-01-31 13:27:09 +0800

    加了钩子

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -10,7 +10,8 @@ using MyFrame.BrainBubbles.Frame.Core;
 using MyFrame.EventSystem.Core;
 using MyFrame.EventSystem.Interfaces;
 using UnityEngine;
-
+using static BasePanelController;
+using LunziSpace;
 
 namespace MyFrame.BrainBubbles.Bubbles.Manager
 {
@@ -18,20 +19,23 @@ namespace MyFrame.BrainBubbles.Bubbles.Manager
     {
         private IEventBusCore _eventBus;
         private RectTransform _frame;
-        private BubblesInfo _bubblesInfo;
-        private IBubbleManager _bubbleManager;
 
-        private BubbleFrame _bubbleFrame;
+         private BubblesInfo _bubblesInfo;
+        private IBubbleManager _bubbleManager;//
+
+        private BubbleFrame _bubbleFrame;//需要new,似乎是泡泡生成的框?
         private BrainSceneManager _brainSceneManager;
+
 
         private void Start()
         {
             // EventSystem Bulid
             _eventBus = new EventBusCore();
             
-            _frame = GameObject.Find("Canvas").GetComponent<RectTransform>();
+            _frame = GameObject.Find("Canvas").GetComponent<RectTransform>();//框的范围
             _brainSceneManager = new BrainSceneManager(_frame, new Vector2Int(-200, -100), new Vector2Int(600, 300));
-            
+
+            UIManager.Instance.DialogPanel.GetComponent<DialogController>().FightAction += BattleInit;//这里放了一个钩子,战斗开始时触发
 
             _brainSceneManager.Start();
         }
@@ -39,6 +43,15 @@ namespace MyFrame.BrainBubbles.Bubbles.Manager
         private void Update()
         {
             _brainSceneManager?.OnUpdate();
+        }
+
+      /// <summary>
+      /// 订阅FightAction
+      /// </summary>
+        public void BattleInit( )
+        {
+
+           
         }
     }
 }
